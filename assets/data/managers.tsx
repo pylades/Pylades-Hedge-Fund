9dc9f725b1d21808069bfb9e1ed0b197f9d7adab
Michael Arnold <arnold.michael91@gmail.com>
2020-12-23 22:58:37 +0900

    refactored into managers.json

--- assets/data/managers.tsx
+++ assets/data/managers.tsx
@@ -4,24 +4,7 @@ import axios from 'axios';
 // - Obj Keys are all vailable game verticals
 // - Managers are stored per vertical
 
-export const managerObject: ManagerObject = {
-  sorare: {
-    vertical: 'sorare',
-    link: 'https://sorare.com/',
-    managers: [
-      {
-        address: '0x90778E7335EF8350a7F2EAe37FC6D549E4ABf095',
-        nickname: 'AJ',
-        verification: true,
-      },
-      {
-        address: '0x84265C1652D6382b6244d958B193ef5895781D5A',
-        nickname: 'Maxmersch',
-        verification: true,
-      },
-    ],
-  },
-};
+export const managerObject: ManagerObject = require('./managers.json');
 
 export const managers = async () => {
   return await axios.get('https://raw.githubusercontent.com/BlackPool-Finance/managers/main/verticals.json');
